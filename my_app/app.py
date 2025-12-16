6bedb12fa43139d819c725a90d1797a306eb00a4
Phu An NGUYEN <nguyenphuan2714@gmail.com>
2025-12-16 16:36:56 +0000

    feat: fix 1

--- my_app/app.py
+++ my_app/app.py
@@ -1,3 +1,9 @@
+import os
+from pathlib import Path
 import streamlit as st
+
+st.write("CWD:", os.getcwd())
+st.write("Exists my_app/assets:", (Path("assets").exists()))
+st.write("Exists ../assets:", (Path("..") / "assets").exists())
 
 st.switch_page("pages/0_Homepage.py")
